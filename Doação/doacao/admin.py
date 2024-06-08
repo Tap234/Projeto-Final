10fb37ddc7bd82da0b75f2465f124622e802548b
Tap234 <caua.ligin@gmail.com>
2024-06-08 18:30:25 -0300

    models

--- Doação/doacao/admin.py
+++ Doação/doacao/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto, Doacao, DoacaoItem, Entrega, Entrega_Item
+
+admin.site.register(Produto)
+admin.site.register(Doacao)
+admin.site.register(DoacaoItem)
+admin.site.register(Entrega)
+admin.site.register(Entrega_Item)
--- Doação/doacao/models.py
+++ Doação/doacao/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+class Produto(models.Model):
+    descricao = models.CharField(max_length=200)
+    unidade_medida = models.CharField(max_length=200)
+    quantidade = models.IntegerField(max_length=200)
+    def __str__(self):
+        return self.descricao
+    objects = models.Manager()
 
-# Create your models here.
+class Doacao(models.Model):
+    data = models.CharField(max_length=200)
+    def __str__(self):
+        return self.data
+    objects = models.Manager()
+
+
+class DoacaoItem(models.Model):
+    doacao = models.ForeignKey(Doacao, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    unidade_medida = models.CharField(max_length=200)
+    quantidade = models.IntegerField(default=0)
+
+class Entrega(models.Model):
+    data = models.CharField(max_length=200)
+    local = models.CharField(max_length=200)
+
+class Entrega_Item(models.Model):
+    entrega = models.ForeignKey(Entrega, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    unidade_medida = models.CharField(max_length=200)
+    quantidade = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.option_text
